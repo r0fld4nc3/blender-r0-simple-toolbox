fdc2efd8b6c2c861e589afdb572844ef1117dd68
Artur <41012911+r0fld4nc3@users.noreply.github.com>
2025-03-31 00:58:56 +0200

    Fix spaghetti rebase (#26)

--- src/r0tools_simple_toolbox/depsgraph.py
+++ src/r0tools_simple_toolbox/depsgraph.py
@@ -14,27 +14,9 @@ def handler_depsgraph_post_update(scene, depsgraph):
     CustomTransformsOrientationsTracker.track_custom_orientations(scene)
 
 
-@bpy.app.handlers.persistent
-def refresh_object_sets_colours(context):
-    """Refresh colors for all object sets"""
-    if u.IS_DEBUG():
-        print("[DEPSGRAPH] Force Refreshing Object Sets' Colours")
-    addon_prefs = u.get_addon_prefs()
-    addon_props = u.get_addon_props()
-    object_sets = addon_props.object_sets
-
-    if not addon_prefs.object_sets_use_colour:
-        return
-
-    for object_set in object_sets:
-        if u.IS_DEBUG():
-            print(f"[DEBUG] [DEPSGRAPH] Refresh: {object_set.name}")
-        object_set.update_object_set_colour(context)
-
-
 depsgraph_handlers = [handler_depsgraph_post_update]
 
-load_post_handlers = [refresh_object_sets_colours]
+load_post_handlers = [u.refresh_object_sets_colours]
 
 
 def register():
