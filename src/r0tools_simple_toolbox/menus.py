0cd2e8bcbc3452177caa4e0037d3bc627d90bed1
Artur <41012911+r0fld4nc3@users.noreply.github.com>
2025-03-30 01:38:23 +0100

    Move refresh method to object sets and fix operator to refresh colours
    * Move refresh object sets colours method to object_sets file
    * Fix bad reference to function call refresh object sets

---
+++ src/r0tools_simple_toolbox/menus.py
@@ -0,0 +1,34 @@
+import bpy
+
+
+class SimpleToolbox_MT_ObjectSetsActionsMenu(bpy.types.Menu):
+    bl_label = "Object Sets Actions"
+
+    def draw(self, context):
+        from .operators import SimpleToolbox_OT_RandomiseObjectSetsColours
+
+        layout = self.layout
+        layout.operator(SimpleToolbox_OT_RandomiseObjectSetsColours.bl_idname)
+
+
+# ===================================================================
+#   Register & Unregister
+# ===================================================================
+
+# fmt: off
+classes = [
+    SimpleToolbox_MT_ObjectSetsActionsMenu,
+]
+# fmt: on
+
+
+def register():
+    for cls in classes:
+        print(f"[MENUS] Register {cls.__name__}")
+        bpy.utils.register_class(cls)
+
+
+def unregister():
+    for cls in classes:
+        print(f"[MENUS] Unregister {cls.__name__}")
+        bpy.utils.unregister_class(cls)
