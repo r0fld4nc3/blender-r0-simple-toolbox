29ef9d41a2a5802fa8bcae300b7449dc6d00d5cc
Artur <41012911+r0fld4nc3@users.noreply.github.com>
2025-03-31 01:12:50 +0200

    Bump dev version

--- src/r0tools_simple_toolbox/__init__.py
+++ src/r0tools_simple_toolbox/__init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "(DEV) r0Tools - Simple Toolbox",
     "author": "Artur Rosário",
-    "version": (0, 0, 25),
+    "version": (0, 0, 26),
     "blender": (4, 2, 0),
     "location": "3D View > Tool",
     "description": "Miscellaneous Utilities",
