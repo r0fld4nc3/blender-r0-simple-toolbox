6ad36a3f7e0fc84fe9397097aa81528a3ad0e70a
Artur <41012911+r0fld4nc3@users.noreply.github.com>
2025-04-04 21:01:36 +0200

    Update Object Sets Action Menu
    * Add Operator to move objects into collections in the Object Sets Action Menu

--- src/r0tools_simple_toolbox/menus.py
+++ src/r0tools_simple_toolbox/menus.py
@@ -8,12 +8,14 @@ class SimpleToolbox_MT_ObjectSetsActionsMenu(bpy.types.Menu):
     def draw(self, context):
         from .operators import (
             SimpleToolbox_OT_ForceRefreshObjectSets,
+            SimpleToolbox_OT_MoveObjectsInObjectSetsToCollections,
             SimpleToolbox_OT_RenameObjectsInObjectSet,
         )
 
         layout = self.layout
         layout.operator(SimpleToolbox_OT_ForceRefreshObjectSets.bl_idname, icon="FILE_REFRESH")
         layout.operator(SimpleToolbox_OT_RenameObjectsInObjectSet.bl_idname, icon="OUTLINER_OB_FONT")
+        layout.operator(SimpleToolbox_OT_MoveObjectsInObjectSetsToCollections.bl_idname, icon="COLLECTION_NEW")
 
 
 # ===================================================================
