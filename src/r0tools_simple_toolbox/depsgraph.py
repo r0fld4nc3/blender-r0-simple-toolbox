538696ad1b22919935336cf1117476fbb84d5717
Artur <41012911+r0fld4nc3@users.noreply.github.com>
2025-06-23 17:39:58 +0200

    Rephrase

--- src/r0tools_simple_toolbox/depsgraph.py
+++ src/r0tools_simple_toolbox/depsgraph.py
@@ -13,7 +13,7 @@ def handler_depsgraph_post_update(scene, depsgraph):
     # Check specifically for object deletions
     if depsgraph.id_type_updated(u.DEPSGRAPH_ID_TYPES.OBJECT):
         if not u.is_writing_context_safe(scene, check_addon_props=True):
-            print(f"[INFO] [{_mod}] We avoided an addon lock crash.")
+            print(f"[INFO] [{_mod}] Avoided an addon state lock.")
             return None
 
         if u.object_count_changed():
