ef7f37984dd51cfc40d3642abc0c9bb13368cd09
Artur <41012911+r0fld4nc3@users.noreply.github.com>
2025-07-06 16:10:14 +0200

    Add hotfix1 tag

--- src/r0tools_simple_toolbox/__init__.py
+++ src/r0tools_simple_toolbox/__init__.py
@@ -1,5 +1,5 @@
 bl_info = {
-    "name": "r0Tools - Simple Toolbox",
+    "name": "r0Tools - Simple Toolbox.hotfix1",
     "author": "Artur Rosário",
     "version": (0, 0, 31),
     "blender": (4, 2, 5),
