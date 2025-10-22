51a5904c787fccc384d0946ef1029866d910350e
Artur <41012911+r0fld4nc3@users.noreply.github.com>
2025-10-22 11:02:49 +0200

    Fix DEBUG define call to function

--- src/r0tools_simple_toolbox/export_ops/ui.py
+++ src/r0tools_simple_toolbox/export_ops/ui.py
@@ -1,7 +1,7 @@
 import bpy
 
 from .. import utils as u
-from ..defines import ADDON_CATEGORY, ADDON_NAME_BARE, DEBUG, IDNAME_EXTRA
+from ..defines import ADDON_CATEGORY, ADDON_NAME_BARE, IDNAME_EXTRA
 from . import _mod as parent_mod
 from .operators import *
 
@@ -53,13 +53,13 @@ def register():
             classes.append(variant)
 
     for cls in classes:
-        if DEBUG:
+        if u.is_debug():
             print(f"[INFO] [{_mod}] Register {cls.__name__}")
         bpy.utils.register_class(cls)
 
 
 def unregister():
     for cls in classes:
-        if DEBUG:
+        if u.is_debug():
             print(f"[INFO] [{_mod}] Unregister {cls.__name__}")
         bpy.utils.unregister_class(cls)
