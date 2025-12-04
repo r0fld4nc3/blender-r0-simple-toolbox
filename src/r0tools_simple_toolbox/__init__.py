5badda9d95fdf6294e033353154bf196323e93f5
Artur <41012911+r0fld4nc3@users.noreply.github.com>
2025-12-04 20:30:13 +0100

    Update versioning to conform to SemVer standards

--- src/r0tools_simple_toolbox/__init__.py
+++ src/r0tools_simple_toolbox/__init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "r0Tools - Simple Toolbox.dev",
     "author": "Artur Rosário",
-    "version": (0, 0, 35),
+    "version": (0, 1, 0),
     "blender": (4, 2, 5),
     "location": "3D View > Simple Toolbox",
     "description": "Miscellaneous Utilities",
