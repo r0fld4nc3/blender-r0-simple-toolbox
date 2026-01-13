7ef134a47a4994c8f3599a2fac9671a789b90f79
Artur <41012911+r0fld4nc3@users.noreply.github.com>
2026-01-13 14:04:36 +0100

    Fix bl_info name from merge

--- src/r0tools_simple_toolbox/__init__.py
+++ src/r0tools_simple_toolbox/__init__.py
@@ -1,5 +1,5 @@
 bl_info = {
-    "name": "r0Tools - Simple Toolbox.logging",
+    "name": "r0Tools - Simple Toolbox.dev",
     "author": "Artur Rosário",
     "version": (0, 4, 0),
     "blender": (4, 2, 5),
