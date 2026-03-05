c386e35bbb635a9489ebe90040034fa1a106db71
Artur <41012911+r0fld4nc3@users.noreply.github.com>
2026-03-05 13:57:27 +0100

    Add pause to rename in build scripts.

--- addon_build/build_extension.sh
+++ addon_build/build_extension.sh
@@ -8,5 +8,8 @@ REPO_DIR="../../blender-addons-repo/release"
 # Build the extension
 "$BLENDER_DIR" -b --factory-startup --command extension build --source-dir "$SOURCE_DIR" --output-dir "$REPO_DIR"
 
+echo -e "\nPress ENTER to continue generating server files. This is your chance to rename the file(s) if needed."
+read -r
+
 # Generate server files
 "$BLENDER_DIR" -b --factory-startup --command extension server-generate --repo-dir "$REPO_DIR"
