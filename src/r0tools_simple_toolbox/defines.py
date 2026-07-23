83e3124ff3cd61a44e94c2c293da84ef01d7ddab
Artur <41012911+r0fld4nc3@users.noreply.github.com>
2026-07-23 17:44:27 +0200

    Update log file path

--- src/r0tools_simple_toolbox/defines.py
+++ src/r0tools_simple_toolbox/defines.py
@@ -36,5 +36,5 @@ INTERNAL_NAME      = _set_addon_internal_name(BASE_NAME)
 REPO_NAME          = "r0Tools Extensions"
 UPDATE_CHECK_CD    = 60  # seconds
 TOOLBOX_PROPS_NAME = "r0fl_toolbox_props"
-LOG_FILE           = Path(bpy.utils.user_resource("DATAFILES")) / "simple_toolbox.log"
+LOG_FILE           = Path(bpy.utils.user_resource("DATAFILES")) / "Simple Toolbox" / "logs" / "simple_toolbox.log"
 # fmt: on
