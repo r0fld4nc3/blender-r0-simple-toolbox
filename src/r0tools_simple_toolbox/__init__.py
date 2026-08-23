a0343532879619ab5fd16e7a9c06bee652d2db04
Artur <41012911+r0fld4nc3@users.noreply.github.com>
2026-08-23 21:03:36 +0200

    Unbump patch version

--- src/r0tools_simple_toolbox/__init__.py
+++ src/r0tools_simple_toolbox/__init__.py
@@ -2,7 +2,7 @@
 bl_info = {
     "name": "r0Tools - Simple Toolbox",
     "author": "Artur Rosário",
-    "version": (0, 5, 3),
+    "version": (0, 5, 2),
     "blender": (4, 5, 0),
     "location": "3D View > Simple Toolbox",
     "description": "General Miscellaneous Quality of Life workflow utilities.",
